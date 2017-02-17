8e592c1e189e0071054078d47ba863656bac7db0
aldo092 <moralescastro8@hotmail.com>
2017-02-17 02:57:26 -0600

    se modifico procedimiento pacientesDoctor agregado campo consultorio en tabla pacientes  modificado procedimiento agregar paciente   agregado procedimiento que devuelve la especialidad    proc BuscarTodasCitas modificado     agregados proc de buscar rol, buscarcitaspormedico, pacientesconsultorio buscarcitas por dia y citas dias medico     modificado procedimiento pacientescosultorio
        generacion de historial clinico y nota de historia clinica de ginecologia

--- admin/Controllers/ctrlAntFam.php
+++ admin/Controllers/ctrlAntFam.php
@@ -34,16 +34,10 @@ $alergias="";
 $convulsiones="";
 $defectosCongenito="";
 $Cancer="";
-$cardiopatias="";
-$TB="";
-$Epilepsia="";
-$InsRen="";
 $oAntFam=new AntecFamiliares();
 $sErr="";
 $nAfec =0;
 $user= $_SESSION['sUser']->getEmail();
-$url="../admin/Sesiones/Pacientes/Pacientes.php";
-
 
 
 if( isset($_POST["nExpediente"])&&!empty($_POST["nExpediente"])&&
@@ -56,11 +50,7 @@ if( isset($_POST["nExpediente"])&&!empty($_POST["nExpediente"])&&
     isset($_POST["alergias"]) && !empty($_POST["alergias"])&&
     isset($_POST["convulsiones"]) && !empty($_POST["convulsiones"])&&
     isset($_POST["congenitos"]) && !empty($_POST["congenitos"])&&
-    isset($_POST["cancer"]) && !empty($_POST["cancer"])&&
-    isset($_POST["cardiopatias"]) && !empty($_POST["cardiopatias"])&&
-    isset($_POST["tb"]) && !empty($_POST["tb"])&&
-    isset($_POST["epilepsia"]) && !empty($_POST["epilepsia"])&&
-    isset($_POST["insren"]) && !empty($_POST["insren"])){
+    isset($_POST["cancer"]) && !empty($_POST["cancer"])) {
 
     $Expediente = $_POST["nExpediente"];
     $Alcoholismo = $_POST["alcoholismo"];
@@ -73,11 +63,6 @@ if( isset($_POST["nExpediente"])&&!empty($_POST["nExpediente"])&&
     $convulsiones = $_POST["convulsiones"];
     $defectosCongenito = $_POST["congenitos"];
     $Cancer = $_POST["cancer"];
-    $cardiopatias=$_POST["cardiopatias"];
-    $TB=$_POST["tb"];
-    $Epilepsia=$_POST["epilepsia"];
-    $InsRen=$_POST["insren"];
-
 
     $oAntFam->setExpediente($Expediente);
     $oAntFam->setAlcoholismo($Alcoholismo);
@@ -90,25 +75,21 @@ if( isset($_POST["nExpediente"])&&!empty($_POST["nExpediente"])&&
     $oAntFam->setConvulsiones($convulsiones);
     $oAntFam->setCongenitos($defectosCongenito);
     $oAntFam->setCancer($Cancer);
-    $oAntFam->setSCardiopatias($cardiopatias);
-    $oAntFam->setSTuberculosis($TB);
-    $oAntFam->setSEpilepsia($Epilepsia);
-    $oAntFam->setSInsRenal($InsRen);
 
     $nAfec=$oAntFam->insertar($user);
 
     if ($nAfec==1){
         $sMsj = "Registro  de antecedentes familiares del expediente ".$Expediente." correcto";
-        header("Location:../mensajes.php?sMensaje=".$sMsj."&Destino=".$url);
+        header("Location:../mensajes.php?sMensaje=".$sMsj);
     } else {
         $sMsj = "Error al guardar los antecedente familiares del expediente".$Expediente;
-        header("Location:../mensajes.php?sMensaje=".$sMsj."&Destino=".$url);
+        header("Location:../mensajes.php?sMensaje=".$sMsj);
 
     }
 
 }else{
     $sMsj = "Faltan datos, registre todos los campos";
-    header("Location:../mensajes.php?sMensaje=".$sMsj."&Destino=".$url);
+    header("Location:../mensajes.php?sMensaje=".$sMsj);
 
 }
 
